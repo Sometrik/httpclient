377fbd49e79f9a5bb7888ac7ec548e0013f1b01c
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-12 18:57:39 +0200

    add FNV

---
+++ src/FNV.h
@@ -0,0 +1,13 @@
+#ifndef _FNV_H_
+#define _FNV_H_
+
+class FNV {
+ public:
+  static long long calcFNV1a_64(const void * ptr, size_t len);
+  static long long calcFNV1a_64(const std::string & s) { return calcFNV1a_64(s.data(), s.size()); }
+  
+  static int calcFNV1a_32(const void * ptr, size_t len);
+  static int calcFNV1a_32(const std::string & s) { return calcFNV1a_32(s.data(), s.size()); }
+};
+
+#endif
