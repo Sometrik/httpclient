5a7cae228fc5d2f61c5d37bacfd3a98041075377
MSuni <mikko.suni@hotmail.com>
2015-12-14 15:33:15 +0200

    added initial request code
    also added java http class and method initializations

--- src/AndroidClient.h
+++ src/AndroidClient.h
@@ -15,7 +15,7 @@ class AndroidClient : public HTTPClient {
  public:
 
 
-	AndroidClient(JNIEnv * _env) : HTTPClient(interface, userAgent, cookieEnable, keepaliveEnable){
+	AndroidClient(JNIEnv * _env) : HTTPClient(interfaceName, userAgent, cookieEnable, keepaliveEnable){
 
 
 		androidInit();
@@ -24,12 +24,26 @@ class AndroidClient : public HTTPClient {
 	void androidInit(){
 
 		cookieManagerClass =  env->FindClass("android/webkit/CookieManager");
+		httpClass = env->FindClass("java/net/HttpURLConnection");
+		urlClass = env->FindClass("java/net/URL");
+
+		urlConstructor =  env->GetMethodID(urlClass, "<init>", "(Ljava/lang/String;)V");
+		OpenConnectionMethod = env->GetMethodID(httpClass, "openConnection", "()java/net/URLConnection;");
+		SetRequestProperty = env->GetMethodID(httpClass, "setRequestProperty", "(Ljava/lang/String;Ljava/lang/String;)V");
+		SetRequestMethod = env->GetMethodID(httpClass, "setRequestMethod", "(Ljava/lang/String;)V");
+		SetDoInputMethod = env->GetMethodID(httpClass, "setDoInput", "(Z)V");
+		connectMethod = env->GetMethodID(httpClass, "connect", "()V");
 		clearCookiesMethod =  env->GetMethodID(cookieManagerClass, "removeAllCookie", "()V");
 
 	}
 
   HTTPResponse request(const HTTPRequest & req, const Authorization & auth){
 
+  	jobject url = env->NewObject(urlClass, urlConstructor, env->NewStringUTF(req.getURI().c_str()));
+
+  	//This would normally be casted. How?
+  	jobject firstConnection = env->CallObjectMethod(url, OpenConnectionMethod);
+  	//jobject httpConnection = firstConnection.jcast(firstConnection, "java/net/HttpURLConnection", JNI_FALSE, JNI_FALSE);
 
 
 
@@ -46,7 +60,7 @@ class AndroidClient : public HTTPClient {
 
 
  private:
-	std::string interface;
+	std::string interfaceName;
 	std::string userAgent;
 	bool cookieEnable;
 	bool keepaliveEnable;
@@ -55,6 +69,15 @@ class AndroidClient : public HTTPClient {
   jclass cookieManagerClass;
   jmethodID clearCookiesMethod;
 
+  jclass urlClass;
+  jmethodID urlConstructor;
+  jmethodID OpenConnectionMethod;
+  jmethodID SetRequestProperty;
+  jmethodID SetRequestMethod;
+  jmethodID SetDoInputMethod;
+  jmethodID connectMethod;
+
+  jclass httpClass;
 
 };
 
