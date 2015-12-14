6994c4a78de716b72eb9020feae821b008b24768
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-14 02:22:17 +0200

    add more fields to HTTPRequest

--- src/HTTPRequest.h
+++ src/HTTPRequest.h
@@ -16,12 +16,24 @@ class HTTPRequest {
 
   const RequestType getType() const { return type; }
   const std::string & getURI() const { return uri; }
-  const std::string & getData() const { return data; }
+  const std::string & getContent() const { return content; }
+  const std::string & getContentType() const { return content_type; }
+  bool getFollowLocation() const { return follow_location; }
+  int getTimeout() const { return timeout; }
+  
+  void setContent(const std::string & _content) { content = _content; }
+  void setContentType(const std::string & _content_type) { content_type = _content_type; }
 
+  void setFollowLocation(bool f) { follow_location = f; }
+  void setTimeout(int t) { timeout = t; }
+  
  private:
   RequestType type;
   std::string uri;
-  std::string data;
+  std::string content;
+  std::string content_type;
+  bool follow_location = true;
+  int timeout = 0;
 };
 
 #endif
