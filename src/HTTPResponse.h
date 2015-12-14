8b698e394ad00694bb6e69d2bed99815262153fe
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-14 02:40:54 +0200

    temporarily remove headers from HTTPResponse

--- src/HTTPResponse.h
+++ src/HTTPResponse.h
@@ -6,6 +6,7 @@
 
 class HTTPResponse {
  public:
+ HTTPResponse() : result_code(0) { }
  HTTPResponse(int _result_code, const std::string & _error_text)
    : result_code(_result_code), error_text(_error_text) { }
  HTTPResponse(int _result_code, const std::string & _error_text, const std::string & _redirect_url, const std::string & _content)
@@ -18,18 +19,22 @@ class HTTPResponse {
   const std::string & getErrorText() { return error_text; }
   int getResultCode() const { return result_code; }
   const std::string & getRedirectUrl() const { return redirect_url; }
+
+#if 0
   const std::map<std::string, std::string> & getHeaders() const { return headers; }
-  
   void addHeader(const char * name, const char * value) {
     headers[name] = value;
   }
+#endif
 
  private:
   int result_code;
   std::string error_text;
   std::string redirect_url;
   std::string content;
-  std::map<std::string, std::string> headers; 
+#if 0
+  std::map<std::string, std::string> headers;
+#endif
 };
 
 #endif
