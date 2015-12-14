3c59e6d0d8111b63aac4613557db074544458d6d
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-14 02:21:59 +0200

    add more fields to HTTPResponse

--- src/HTTPResponse.h
+++ src/HTTPResponse.h
@@ -1,11 +1,35 @@
 #ifndef _HTTPRESPONSE_H_
 #define _HTTPRESPONSE_H_
 
+#include <string>
+#include <map>
+
 class HTTPResponse {
  public:
+ HTTPResponse(int _result_code, const std::string & _error_text)
+   : result_code(_result_code), error_text(_error_text) { }
+ HTTPResponse(int _result_code, const std::string & _error_text, const std::string & _redirect_url, const std::string & _content)
+   : result_code(_result_code), error_text(_error_text), redirect_url(_redirect_url), content(_content) { }
+  
+  bool isSuccess() const { return result_code == 200; }
+  bool isError() const { return result_code != 200; }
+
+  const std::string & getContent() { return content; }
+  const std::string & getErrorText() { return error_text; }
+  int getResultCode() const { return result_code; }
+  const std::string & getRedirectUrl() const { return redirect_url; }
+  const std::map<std::string, std::string> & getHeaders() const { return headers; }
+  
+  void addHeader(const char * name, const char * value) {
+    headers[name] = value;
+  }
 
  private:
   int result_code;
+  std::string error_text;
+  std::string redirect_url;
+  std::string content;
+  std::map<std::string, std::string> headers; 
 };
 
 #endif
