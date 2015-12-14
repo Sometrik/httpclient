d730d14f16452fd46b76f2943ee21d475ca6936a
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-14 02:40:34 +0200

    add headers to HTTPRequest

--- src/HTTPRequest.h
+++ src/HTTPRequest.h
@@ -2,6 +2,7 @@
 #define _HTTPREQUEST_H_
 
 #include <string>
+#include <map>
 
 class HTTPRequest {
  public:
@@ -20,18 +21,24 @@ class HTTPRequest {
   const std::string & getContentType() const { return content_type; }
   bool getFollowLocation() const { return follow_location; }
   int getTimeout() const { return timeout; }
+  const std::map<std::string, std::string> & getHeaders() const { return headers; }
   
   void setContent(const std::string & _content) { content = _content; }
   void setContentType(const std::string & _content_type) { content_type = _content_type; }
 
   void setFollowLocation(bool f) { follow_location = f; }
   void setTimeout(int t) { timeout = t; }
-  
+
+  void addHeader(const char * name, const char * value) {
+    headers[name] = value;
+  }
+
  private:
   RequestType type;
   std::string uri;
   std::string content;
   std::string content_type;
+  std::map<std::string, std::string> headers; 
   bool follow_location = true;
   int timeout = 0;
 };
