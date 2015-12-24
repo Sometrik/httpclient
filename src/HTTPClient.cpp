93db807aa4cc3eac2082747cab558d9b663d69ca
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-24 14:42:42 +0200

    add commented version of IDN conversion

--- src/HTTPClient.cpp
+++ src/HTTPClient.cpp
@@ -4,3 +4,32 @@
 #include <iostream>
 
 using namespace std;
+
+#if 0
+string
+HTTPClient::encodeIDN(const string & input) {
+  URI uri(input);
+  
+  bool ascii = true;
+  for (unsigned int i = 0; i < uri.getDomain().size(); i++) {
+    if (uri.getDomain()[i] < 0) {
+      ascii = false;
+      break;
+    }
+  }
+  if (ascii) {
+    return input;
+  } else {
+    vector<string> parts = StringUtils::split(uri.getDomain(), '.');
+    string output;
+    for (vector<string>::iterator it = parts.begin(); it != parts.end(); it++) {
+      if (it != parts.begin()) output += '.';
+      output += StringUtils::encodePunycode(*it);
+    }
+    cerr << "domain converted to " << output << endl;
+    uri.setDomain(output);
+    return uri.toString();
+  }
+}
+#endif
+
