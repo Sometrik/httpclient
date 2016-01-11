ae48ada36c752ac75938913fcee39598b7fb6f83
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-11 08:07:42 +0200

    add accessor for default_user_agent

--- include/HTTPClient.h
+++ include/HTTPClient.h
@@ -89,6 +89,8 @@ class HTTPClientFactory {
   virtual std::shared_ptr<HTTPClient> createClient(const std::string & _user_agent, bool _enable_cookies = true, bool _enable_keepalive = true) = 0;
   std::shared_ptr<HTTPClient> createClient(bool _enable_cookies = true, bool _enable_keepalive = true) { return createClient(default_user_agent, _enable_cookies, _enable_keepalive); }
 
+  const std::string & getDefaultUserAgent() const { return default_user_agent; }
+  
  private:
   std::string default_user_agent;
 };
