7824dd9865b75436223ffd8a03d5660491a01c12
Mikael Rekola <mikael.rekola@gmail.com>
2016-04-10 23:26:40 +0300

    disable release calls, add headers, fix result code

--- src/iOSClient.mm
+++ src/iOSClient.mm
@@ -29,50 +29,77 @@ class iOSClient : public HTTPClient {
 				    cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
 				    timeoutInterval:req.getTimeout()
 				    ];
-    [url_string release];
+    // [url_string release];
     
     [request setHTTPShouldHandleCookies:NO];
     [request setHTTPShouldUsePipelining:NO];
     
     for (auto & hd : req.getHeaders()) {
+        NSString *n = [NSString stringWithUTF8String:hd.first.c_str()];
+        NSString *v = [NSString stringWithUTF8String:hd.second.c_str()];
+        [request setValue:v forHTTPHeaderField:n];
     }
   
     if (req.getType() == HTTPRequest::POST) {
       [request setHTTPMethod: @"POST"];
       NSString *post_size = [NSString stringWithFormat:@"%d", (int)req.getContent().size()];
       [request setValue:post_size forHTTPHeaderField:@"Content-Length"];
-      [post_size release];
+      // [post_size release];
 
       NSString *content_type = [NSString stringWithUTF8String:req.getContentType().c_str()];
       [request setValue:content_type forHTTPHeaderField:@"Content-Type"];
-      [content_type release];
+      // [content_type release];
 
       NSData * post_data = [NSData dataWithBytes:req.getContent().data() length:req.getContent().size()];
       [request setHTTPBody:post_data];
-      [post_data release];
+      // [post_data release];
     } else {
       [request setHTTPMethod: @"GET"];
     }
+
+    const BasicAuth * basic = dynamic_cast<const BasicAuth *>(&auth);
+    if (basic) {
+#if 0
+      string s = basic->getUsername();
+      s += ':';
+      s += basic->getPassword();
+#endif
+    } else {
+      string auth_header = auth.createHeader();
+      if (!auth_header.empty()) {
+	string s = auth.getHeaderName();
+	s += ": ";
+	s += auth_header;
+        NSString *n = [NSString stringWithUTF8String:auth.getHeaderName()];
+        NSString *v = [NSString stringWithUTF8String:auth_header.c_str()];
+        [request setValue:v forHTTPHeaderField:n];
+      }
+    }
+
     // set follow location
     // set cookies
     NSString* user_agent_string = [NSString stringWithUTF8String: user_agent.c_str()];
     [request setValue:user_agent_string forHTTPHeaderField:@"User-Agent"];
-    [user_agent_string release];
+    // [user_agent_string release];
     
     NSError *requestError = nil;
     NSURLResponse *urlResponse = nil;
     NSData *responseData = [NSURLConnection sendSynchronousRequest:request
 			    returningResponse:&urlResponse error:&requestError];
-
-    int result_code = [urlResponse statusCode];
+    int result_code = 0;
+    if (requestError) {
+      result_code = [requestError code];
+    } else if ([urlResponse isKindOfClass:[NSHTTPURLResponse class]]) {
+      result_code = [(NSHTTPURLResponse *)urlResponse statusCode];
+    }
     string content((const char *)[responseData bytes], [responseData length]);
     // get redirect_url
     // NSDictionary* headers = [(NSHTTPURLResponse *)response allHeaderFields];
     
-    [request release];
-    [responseData release];
-    [requestError release];
-    [urlResponse release];
+    // [request release];
+    // [responseData release];
+    // [requestError release];
+    // [urlResponse release];
 
     return HTTPResponse(result_code, "", "", content);
   }
