3ceeec1fa09341006d02af4159c1b473f4692372
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-16 04:33:18 +0200

    remove callback member variable, add it as a method parameter, add noAuth member variable, add new version of Get()

--- include/HTTPClient.h
+++ include/HTTPClient.h
@@ -18,8 +18,7 @@ class HTTPClient {
     enable_keepalive(_enable_keepalive)
     { }  
   HTTPClient(const HTTPClient & other)
-    : callback(other.callback),
-    user_agent(other.user_agent),
+    : user_agent(other.user_agent),
     cookie_jar(other.cookie_jar),
     enable_cookies(other.enable_cookies),
     enable_keepalive(other.enable_keepalive)
@@ -33,14 +32,14 @@ class HTTPClient {
     HTTPRequest req(HTTPRequest::POST, uri);
     req.setContent(data);
     req.setContentType("application/x-www-form-urlencoded;charset=UTF-8");
-    return request(req, Authorization::noAuth);
+    return request(req, noAuth);
   }
 
-  HTTPResponse Post(const std::string & uri, const std::string & data, const Authorization & auth) {
+  HTTPResponse Post(const std::string & uri, const std::string & data, const Authorization & auth, HTTPClientInterface * callback = 0) {
     HTTPRequest req(HTTPRequest::POST, uri);
     req.setContent(data);
     req.setContentType("application/x-www-form-urlencoded;charset=UTF-8");
-    return request(req, auth);
+    return request(req, auth, callback);
   }
   
   HTTPResponse Get(const std::string & uri, const std::string & data, const Authorization & auth, bool follow_location = true, int timeout = 0) {
@@ -52,12 +51,22 @@ class HTTPClient {
     req.setTimeout(timeout);
     return request(req, auth);
   }
-  
-  HTTPResponse Get(const std::string & uri, bool follow_location = true, int timeout = 0) {
+
+  HTTPResponse Get(const std::string & uri, const std::string & data, bool follow_location = true, int timeout = 0, HTTPClientInterface * callback = 0) {
+    std::string uri2 = uri;
+    uri2 += '?';
+    uri2 += data;
+    HTTPRequest req(HTTPRequest::GET, uri2);
+    req.setFollowLocation(follow_location);
+    req.setTimeout(timeout);
+    return request(req, noAuth, callback);
+  }
+
+  HTTPResponse Get(const std::string & uri, bool follow_location = true, int timeout = 0, HTTPClientInterface * callback = 0) {
     HTTPRequest req(HTTPRequest::GET, uri);
     req.setFollowLocation(follow_location);
     req.setTimeout(timeout);
-    return request(req, Authorization::noAuth);
+    return request(req, noAuth, callback);
   }
   
   HTTPResponse Get(const std::string & uri, const Authorization & auth, bool follow_location = true, int timeout = 0) {
@@ -68,13 +77,13 @@ class HTTPClient {
   }
 
   void setCookieJar(const std::string & filename) { cookie_jar = filename; }
-  void setCallback(HTTPClientInterface * _callback) { callback = _callback; }
 
-  virtual HTTPResponse request(const HTTPRequest & req, const Authorization & auth) = 0;
+  virtual HTTPResponse request(const HTTPRequest & req, const Authorization & auth, HTTPClientInterface * callback = 0) = 0;
   virtual void clearCookies() = 0;
-        
+
+  Authorization noAuth;
+
  protected:
-  HTTPClientInterface * callback = 0;
   std::string user_agent;
   std::string cookie_jar;
   bool enable_cookies, enable_keepalive;
