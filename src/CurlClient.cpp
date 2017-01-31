03934edb662894cfdea6c26b95638a12bd855b5b
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-31 14:06:09 +0200

    don't reset options, refactor

--- src/CurlClient.cpp
+++ src/CurlClient.cpp
@@ -15,6 +15,10 @@ using namespace std;
 
 static bool is_initialized = false;
 
+static size_t write_data_func(void * buffer, size_t size, size_t nmemb, void * userp);
+static size_t headers_func(void * buffer, size_t size, size_t nmemb, void *userp);
+static int progress_func(void * clientp, double dltotal, double dlnow, double ultotal, double ulnow);
+
 class CurlClient : public HTTPClient {
  public:
   CurlClient(const std::string & _interface, const std::string & _user_agent, bool _enable_cookies = true, bool _enable_keepalive = true)
@@ -59,7 +63,6 @@ class CurlClient : public HTTPClient {
       string s = basic->getUsername();
       s += ':';
       s += basic->getPassword();
-      curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
       curl_easy_setopt(curl, CURLOPT_USERPWD, s.c_str());
     } else {
       string auth_header = auth.createHeader();
@@ -84,68 +87,46 @@ class CurlClient : public HTTPClient {
       s += hd.second;
       headers = curl_slist_append(headers, s.c_str());
     }
-
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent.c_str());
-    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_func);
-    curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_func);
-    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, headers_func);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30);
-    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
-    curl_easy_setopt(curl, CURLOPT_DNS_CACHE_TIMEOUT, 600);
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-    curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "gzip, deflate");
-    if (!enable_keepalive) {
-      curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1);
-      curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 1);
-      // curl_easy_setopt(curl, CURLOPT_MAXCONNECTS, 10);
-    }
-    if (!cookie_jar.empty()) {
-      curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookie_jar.c_str());
-      curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookie_jar.c_str());
-    } else if (enable_cookies) {
-      curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
-    }
     
     if (req.getType() == HTTPRequest::POST) {
+      curl_easy_setopt(curl, CURLOPT_HTTPGET, 0);
       curl_easy_setopt(curl, CURLOPT_POST, 1);
       curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, req.getContent().size());
       curl_easy_setopt(curl, CURLOPT_POSTFIELDS, req.getContent().data());
     } else {
+      curl_easy_setopt(curl, CURLOPT_POST, 0);
       curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
     }
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, req.getFollowLocation());
     curl_easy_setopt(curl, CURLOPT_URL, req.getURI().c_str());
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30);
     curl_easy_setopt(curl, CURLOPT_TIMEOUT, req.getTimeout());
-      
+    
     curl_easy_setopt(curl, CURLOPT_HEADERDATA, &callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &callback);
     curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, &callback);
-    if (!cookie_jar.empty()) {
-      curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookie_jar.c_str());
-      curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookie_jar.c_str());
-    } 
 
     CURLcode res = curl_easy_perform(curl);
     // if (res != 0) {
     //   response.setResultCode(500);
     //   response.setErrorText(curl_easy_strerror(res));
     // }
-
+    
     callback.handleDisconnect();
-
+    
     if (headers) {
       curl_easy_setopt(curl, CURLOPT_HTTPHEADER, 0);
       curl_slist_free_all(headers);
     }
-
+    
     curl_easy_setopt(curl, CURLOPT_HEADERDATA, 0);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, 0);
     curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, 0);
-
-    // return response;
+    curl_easy_setopt(curl, CURLOPT_USERPWD, 0);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, 0);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, 0);
   }
   
   void clearCookies() override {
@@ -156,72 +137,93 @@ class CurlClient : public HTTPClient {
   
  protected:
   bool initialize() {
-    if (curl) {
-      curl_easy_reset(curl);
-    } else {
+    if (!curl) {
       curl = curl_easy_init();
-    }
-    
-    assert(curl);
-    
-    if (curl) {
+      assert(curl);
+      if (curl) {
 #ifndef WIN32
-      if (!interface_name.empty()) {
-	int r = curl_easy_setopt(curl, CURLOPT_INTERFACE, interface_name.c_str());
-	assert(r != CURLE_INTERFACE_FAILED);
-      }
+	if (!interface_name.empty()) {
+	  int r = curl_easy_setopt(curl, CURLOPT_INTERFACE, interface_name.c_str());
+	  assert(r != CURLE_INTERFACE_FAILED);
+	}
 #endif
+	curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent.c_str());
+	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_func);
+	curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_func);
+	curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, headers_func);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
+	curl_easy_setopt(curl, CURLOPT_DNS_CACHE_TIMEOUT, 600);
+	curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
+	curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "gzip, deflate");
+
+	if (!cookie_jar.empty()) {
+	  curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookie_jar.c_str());
+	  curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookie_jar.c_str());
+	} else if (enable_cookies) {
+	  curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
+	}
+	if (!enable_keepalive) {
+	  curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1);
+	  curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 1);
+	  // curl_easy_setopt(curl, CURLOPT_MAXCONNECTS, 10);
+	}
+      }
     }
     
     return curl != 0;
   }
 
  private:
+#if 0
   static size_t write_data_func(void * buffer, size_t size, size_t nmemb, void *userp);
   static size_t headers_func(void * buffer, size_t size, size_t nmemb, void *userp);
   static int progress_func(void *  clientp, double dltotal, double dlnow, double ultotal, double ulnow);
+#endif
 
   std::string interface_name;
   CURL * curl = 0;
 };
   
-size_t
-CurlClient::write_data_func(void * buffer, size_t size, size_t nmemb, void * userp) {
+static size_t write_data_func(void * buffer, size_t size, size_t nmemb, void * userp) {
   size_t s = size * nmemb;
   // Content-Type: multipart/x-mixed-replace; boundary=myboundary
 					     
   HTTPClientInterface * callback = (HTTPClientInterface *)(userp);
   assert(callback);
-  return callback->handleChunk(s, (const char *)buffer) ? s : 0;  
+  bool t = callback->handleChunk(s, (const char *)buffer);
+  return t ? s : 0;  
 }
 
-size_t
-CurlClient::headers_func(void * buffer, size_t size, size_t nmemb, void *userp) {
+static size_t headers_func(void * buffer, size_t size, size_t nmemb, void *userp) {
   HTTPClientInterface * callback = (HTTPClientInterface *)(userp);
-
+  
+  size_t s = size * nmemb;
   bool keep_running = true;
   if (buffer) {
-    string s((const char*)buffer, size * nmemb);
+    string input((const char*)buffer, s);
 
-    if (s.compare(0, 5, "HTTP/") == 0) {
-      auto pos1 = s.find_first_of(' ');
+    if (input.compare(0, 5, "HTTP/") == 0) {
+      auto pos1 = input.find_first_of(' ');
       if (pos1 != string::npos) {
-	auto pos2 = s.find_first_of(' ', pos1 + 1);
+	auto pos2 = input.find_first_of(' ', pos1 + 1);
 	if (pos2 != string::npos) {
-	  auto s2 = s.substr(pos1, pos2 - pos1);
+	  auto s2 = input.substr(pos1, pos2 - pos1);
 	  int code = atoi(s2.c_str());
 	  callback->handleResultCode(code);
 	}
       }
     } else {
       int pos1 = 0;
-      for ( ; pos1 < s.size() && s[pos1] != ':'; pos1++) { }
-      int pos2 = s[pos1] == ':' ? pos1 + 1 : pos1;
-      for (; pos2 < s.size() && isspace(s[pos2]); pos2++) { }
-      int pos3 = s.size();
-      for (; pos3 > 0 && isspace(s[pos3 - 1]); pos3--) { }
-      std::string key = s.substr(0, pos1);
-      std::string value = s.substr(pos2, pos3 - pos2);
+      for ( ; pos1 < input.size() && input[pos1] != ':'; pos1++) { }
+      int pos2 = input[pos1] == ':' ? pos1 + 1 : pos1;
+      for (; pos2 < input.size() && isspace(input[pos2]); pos2++) { }
+      int pos3 = input.size();
+      for (; pos3 > 0 && isspace(input[pos3 - 1]); pos3--) { }
+      std::string key = input.substr(0, pos1);
+      std::string value = input.substr(pos2, pos3 - pos2);
 
       if (key == "Location" && !callback->handleRedirectUrl(value)) {
 	keep_running = false;
@@ -229,12 +231,11 @@ CurlClient::headers_func(void * buffer, size_t size, size_t nmemb, void *userp) 
       callback->handleHeader(key, value);
     }
   }
-  
-  return keep_running ? size * nmemb : 0;
+
+  return keep_running ? s : 0;
 }
 
-int
-CurlClient::progress_func(void * clientp, double dltotal, double dlnow, double ultotal, double ulnow) {
+static int progress_func(void * clientp, double dltotal, double dlnow, double ultotal, double ulnow) {
   HTTPClientInterface * callback = (HTTPClientInterface *)(clientp);
   assert(callback);
   return callback->onIdle(true) ? 0 : 1;
