c04e22d2deb9ef478b200b451a656bd9c960b031
Mikael Rekola <mikael.rekola@gmail.com>
2017-06-27 03:53:26 +0300

    replace timeout with connect timeout and read timeout

--- include/HTTPRequest.h
+++ include/HTTPRequest.h
@@ -30,15 +30,17 @@ class HTTPRequest {
   const std::string & getContent() const { return content; }
   const std::string & getContentType() const { return content_type; }
   bool getFollowLocation() const { return follow_location; }
-  int getTimeout() const { return timeout; }
+  int getConnectTimeout() const { return connect_timeout; }
+  int getReadTimeout() const { return read_timeout; }
   const std::map<std::string, std::string> & getHeaders() const { return headers; }
   
   void setContent(const std::string & _content) { content = _content; }
   void setContentType(const std::string & _content_type) { content_type = _content_type; }
 
   void setFollowLocation(bool f) { follow_location = f; }
-  void setTimeout(int t) { timeout = t; }
-
+  void setConnectTimeout(int t) { connect_timeout = t; }
+  void setReadTimeout(int t) { read_timeout = t; }
+  
   void addHeader(const char * name, const char * value) {
     headers[name] = value;
   }
@@ -50,7 +52,7 @@ class HTTPRequest {
   std::string content_type;
   std::map<std::string, std::string> headers; 
   bool follow_location = true;
-  int timeout = 0;
+  int connect_timeout = 0, read_timeout = 0;
 };
 
 #endif
