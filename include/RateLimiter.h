21efc3fd54088ee9600fdedb1b8512cd8a22ff73
Mikael Rekola <mikael.rekola@gmail.com>
2017-08-02 23:44:55 +0300

    include unordered_map and add instance

--- include/RateLimiter.h
+++ include/RateLimiter.h
@@ -1,6 +1,8 @@
 #ifndef _RATELIMITER_H_
 #define _RATELIMITER_H_
 
+#include <unordered_map>
+
 struct endpoint_data_s {
   
 };
@@ -17,6 +19,8 @@ class RateLimiter {
  private:
   RateLimiter() { }
 
+  static RateLimiter * instance;
+  
   std::unordered_map<std::string, endpoint_data_s> endpoints;
 };
 
