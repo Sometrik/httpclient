0bbfac834f5762bb83a4782cb50cfcaeba4aa5f4
rekola <mikael.rekola@gmail.com>
2022-03-08 21:54:04 +0200

    Improve WinHTTP implementation

--- src/WinHTTPClient.cpp
+++ src/WinHTTPClient.cpp
@@ -15,31 +15,28 @@
 #endif
 
 #include <iostream>
+#include <URI.h>
+#include <winhttp.h>
 
 using namespace std;
 
-#include <sys/time.h>
-#include <winhttp.h>
-
-static bool is_initialized = false;
-
 static wstring from_utf8(const std::string & s) {
-  wchar_t tmp[s.size() + 1];
-  auto r = MultiByteToWideChar(CP_UTF8, 0, s.data(), -1, tmp, s.size() + 1);
-  return wstring(tmp, static_cast<size_t>(r));
+  std::unique_ptr<wchar_t[]> tmp(new wchar_t[s.size() + 1]);
+  auto r = MultiByteToWideChar(CP_UTF8, 0, s.data(), -1, tmp.get(), s.size() + 1);
+  return wstring(tmp.get(), static_cast<size_t>(r));
 }
 
 static string to_utf8(const wstring & s) {
-  char tmp[4*s.size()+1];
+  std::unique_ptr<char[]> tmp(new char[4 * s.size() + 1]);
   auto r = WideCharToMultiByte(CP_UTF8,
-			       0
+			       0,
 			       s.data(),
 			       -1,
-			       tmp,
+			       tmp.get(),
 			       4*s.size() + 1,
 			       NULL,
 			       NULL);
-  return string(tmp, static_cast<size_t>(r));  
+  return string(tmp.get(), static_cast<size_t>(r));  
 }
 
 static long long get_current_time_ms() {
@@ -63,26 +60,21 @@ static long long get_current_time_ms() {
 class WinHTTPClient : public HTTPClient {
  public:
   WinHTTPClient(const std::string & user_agent, bool enable_cookies = true, bool enable_keepalive = true)
-    : HTTPClient(user_agent, enable_cookies, enable_keepalive),
+    : HTTPClient(user_agent, enable_cookies, enable_keepalive)
   {
-    assert(is_initialized_);
   }
 
   ~WinHTTPClient() {
   }
 
   void request(const HTTPRequest & req, const Authorization & auth, HTTPClientInterface & callback) override {
-    if (!initialize()) {
-      return;
-    }
-
     URI uri(req.getURI());
     
     auto ua_text = from_utf8(getUserAgent());
     auto target_scheme = uri.getScheme();
     auto target_host = from_utf8(uri.getDomain());
     auto target_port = uri.getPort();
-    auto target_path = uri.getPath();
+    auto target_path = from_utf8(uri.getPath());
 
     bool is_secure = false;
     if (!target_port) {
@@ -97,13 +89,13 @@ class WinHTTPClient : public HTTPClient {
     }
         
     // Use WinHttpOpen to obtain a session handle.
-    auto hSession = WinHttpOpen(ua_text,
+    auto hSession = WinHttpOpen(ua_text.c_str(),
 				WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
 				WINHTTP_NO_PROXY_NAME,
 				WINHTTP_NO_PROXY_BYPASS, 0);
     if (hSession) {
       // Specify an HTTP server.
-      auto hConnect = WinHttpConnect(hSession, target_host, target_port, 0);
+      auto hConnect = WinHttpConnect(hSession, target_host.c_str(), target_port, 0);
       if (hConnect) {
 	wstring request_type;
 	bool has_data = true;
@@ -123,7 +115,7 @@ class WinHTTPClient : public HTTPClient {
 	DWORD dwFlags = WINHTTP_FLAG_ESCAPE_DISABLE | WINHTTP_FLAG_ESCAPE_DISABLE_QUERY;
 	if (is_secure) dwFlags |= WINHTTP_FLAG_SECURE;
 	  
-	auto hRequest = WinHttpOpenRequest(hConnect, request_type, target_path, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, dwFlags);
+	auto hRequest = WinHttpOpenRequest(hConnect, request_type.c_str(), target_path.c_str(), NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, dwFlags);
 	if (hRequest) {
 	  map<string, string> combined_headers;
 	  for (auto & hd : default_headers) {
@@ -163,32 +155,12 @@ class WinHTTPClient : public HTTPClient {
 	      // Allocate buffer by header length
 	      // If the function fails and ERROR_INSUFFICIENT_BUFFER is returned, lpdwBufferLength specifies the number of bytes that the application must allocate to receive the string.
 	      if (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
-		lpHeadBuffer = new wchar_t[dwSize / sizeof(wchar_t)];
-		bResults = WinHttpQueryHeaders(hRequest,WINHTTP_QUERY_RAW_HEADERS_CRLF,WINHTTP_HEADER_NAME_BY_INDEX, lpHeadBuffer, &dwSize,WINHTTP_NO_HEADER_INDEX);
+		unique_ptr<wchar_t[]> lpHeadBuffer(new wchar_t[dwSize / sizeof(wchar_t)]);
+		if (WinHttpQueryHeaders(hRequest, WINHTTP_QUERY_RAW_HEADERS_CRLF, WINHTTP_HEADER_NAME_BY_INDEX, lpHeadBuffer.get(), &headerSize, WINHTTP_NO_HEADER_INDEX)) {
+
+  		}	
 	      }
-	      // Get Content-Type from header to specify encoding
-	      if ( NULL != wcsstr(lpHeadBuffer, L"charset=gbk") )
-		{
-		  bUTF8Code = FALSE;
-		}
 	    }
-	    else
-	      {
-		cout << "Get header failed!" << endl;
-	      }
-	  }
-    
-#if 0
-	    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, req.getFollowLocation() ? 1 : 0);
-	    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, req.getConnectTimeout());
-	    
-	    long long current_time_ms = get_current_time_ms();
-	    curl_context_s context = { current_time_ms, current_time_ms, 0, req.getReadTimeout(), req.getConnectionTimeout(), &callback };
-	    
-	    curl_easy_setopt(curl, CURLOPT_HEADERDATA, &context);
-	    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &context);
-	    curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, &context);
-#endif
 	    
 	    callback.handleDisconnect();
 	  }
@@ -202,75 +174,14 @@ class WinHTTPClient : public HTTPClient {
   }
   
   void clearCookies() override {
-    if (curl) {
-      curl_easy_setopt(curl, CURLOPT_URL, "ALL"); // what does this do?
-    }
   }
   
  protected:
-  bool initialize() {
-    if (!curl) {
-      curl = curl_easy_init();
-      assert(curl);
-      if (curl) {
-#ifndef WIN32
-	if (!interface_name.empty()) {
-	  int r = curl_easy_setopt(curl, CURLOPT_INTERFACE, interface_name.c_str());
-	  assert(r != CURLE_INTERFACE_FAILED);
-	}
-#endif
-	curl_easy_setopt(curl, CURLOPT_SHARE, share);
-	curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent.c_str());
-	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_func);
-	curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_func);
-	curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, headers_func);
-	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
-	curl_easy_setopt(curl, CURLOPT_DNS_CACHE_TIMEOUT, 300);
-	curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
-	curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "gzip, deflate");
-
-	if (!cookie_jar.empty()) {
-	  curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookie_jar.c_str());
-	  curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookie_jar.c_str());
-	} else if (enable_cookies) {
-	  curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
-	}
-	if (!enable_keepalive) {
-	  curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1);
-	  curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 1);
-	  // curl_easy_setopt(curl, CURLOPT_MAXCONNECTS, 10);
-	}
-      }
-    }
-    
-    return curl != 0;
-  }
-
- private:
-  static size_t write_data_func(void * buffer, size_t size, size_t nmemb, void *userp);
-  static size_t headers_func(void * buffer, size_t size, size_t nmemb, void *userp);
-  static int progress_func(void *  clientp, double dltotal, double dlnow, double ultotal, double ulnow);
-
-  std::string interface_name;
-  CURL * curl = 0;
-};
+ 
+ };
   
 
 std::unique_ptr<HTTPClient>
 WinHTTPClientFactory::createClient2(const std::string & _user_agent, bool _enable_cookies, bool _enable_keepalive) {
-  return std::unique_ptr<WinHTTPClient>(new WinHTTPClient("", _user_agent, _enable_cookies, _enable_keepalive));
-}
-
-void
-WinHTTPClientFactory::globalInit() {
-  is_initialized = true;
-}
-
-void
-WinHTTPClientFactory::globalCleanup() {
-  is_initialized = false;
+  return std::unique_ptr<WinHTTPClient>(new WinHTTPClient(_user_agent, _enable_cookies, _enable_keepalive));
 }
