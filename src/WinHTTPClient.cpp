8c6cb47cad6199ad54a38962f44beef7385dcf02
rekola <mikael.rekola@gmail.com>
2022-03-13 23:33:09 +0200

    Add mutexes to logging context, improve logging

--- src/WinHTTPClient.cpp
+++ src/WinHTTPClient.cpp
@@ -17,6 +17,7 @@
 #include <iostream>
 #include <URI.h>
 #include <winhttp.h>
+#include <mutex>
 
 using namespace std;
 
@@ -39,19 +40,31 @@ static string to_utf8(const wstring & s) {
   return string(tmp.get(), static_cast<size_t>(r));  
 }
 
-struct winhttp_context_s {
-  // mutex
-  std::string log;
+struct winhttp_status_context_s {
+public:
+  void log(const std::string& s) {
+    std::lock_guard<std::mutex> guard(mutex_);
+    log_ += s;
+  }
+  std::string getLog() const {
+    std::string tmp;
+    {
+      std::lock_guard<std::mutex> guard(mutex_);
+      tmp = log_;
+    }
+    return tmp;
+  }
+
+private:
+  std::string log_;
+  mutable std::mutex mutex_;
 };
 
 static void __stdcall winhttp_status_callback(HINTERNET handle,
-					      DWORD_PTR context,
+					      DWORD_PTR context0,
 					      DWORD status,
 					      void* info,
 					      DWORD info_len) {
-
-  auto * context = reinterpret_cast<winhttp_status_context_s*>(context);
-  
   std::string status_string, info_string;
  
   switch (status) {
@@ -63,14 +76,14 @@ static void __stdcall winhttp_status_callback(HINTERNET handle,
     break;
   case WINHTTP_CALLBACK_STATUS_RESOLVING_NAME:
     status_string = "resolving";
-    info_string = to_utf8(wstring(static_cast<wchar_t*>(info), info_len / sizeof(wchar_t)));
+    info_string = to_utf8(wstring(static_cast<wchar_t*>(info), info_len - 1));
     break;
   case WINHTTP_CALLBACK_STATUS_NAME_RESOLVED:
     status_string = "resolved";
     break;
   case WINHTTP_CALLBACK_STATUS_CONNECTING_TO_SERVER:
     status_string = "connecting";
-    info_string = to_utf8(wstring(static_cast<wchar_t*>(info), info_len / sizeof(wchar_t)));
+    info_string = to_utf8(wstring(static_cast<wchar_t*>(info), info_len - 1));
     break;
   case WINHTTP_CALLBACK_STATUS_CONNECTED_TO_SERVER:
     status_string = "connected";
@@ -95,7 +108,7 @@ static void __stdcall winhttp_status_callback(HINTERNET handle,
     break;
   case WINHTTP_CALLBACK_STATUS_REDIRECT:
     status_string = "redirect";
-    info_string = to_utf8(wstring(static_cast<wchar_t*>(info), info_len / sizeof(wchar_t)));
+    info_string = to_utf8(wstring(static_cast<wchar_t*>(info), info_len - 1));
     break;
   case WINHTTP_CALLBACK_STATUS_DATA_AVAILABLE:
     status_string = "data available";
@@ -128,9 +141,17 @@ static void __stdcall winhttp_status_callback(HINTERNET handle,
   std::string msg;
   if (!status_string.empty()) msg += "status=" + status_string;
   else msg += "status=" + to_string(status);
-  if (!info_string.empty()) msg += ", info=" + info_string;
-  
-  context->log += msg + "\r\n";
+  if (!info_string.empty()) msg += ", info(" + to_string(info_len) + ") = " + info_string;
+
+  for (auto& c : msg) if (c == 0) c = '?';
+
+  if (context0) {
+    auto* context = reinterpret_cast<winhttp_status_context_s*>(context0);
+    context->log(msg + "\r\n");
+  }
+
+  string tmp = "status: " + msg + "\r\n";
+  OutputDebugStringA(tmp.c_str());
 }
 
 class WinHTTPClient : public HTTPClient {
@@ -148,11 +169,14 @@ class WinHTTPClient : public HTTPClient {
      DWORD decompression = WINHTTP_DECOMPRESSION_FLAG_ALL;
      WinHttpSetOption(session_, WINHTTP_OPTION_DECOMPRESSION, &decompression, sizeof(decompression));
 
-#if 0
+#if 1
      DWORD protocols = WINHTTP_PROTOCOL_FLAG_HTTP2;
      WinHttpSetOption(session_, WINHTTP_OPTION_ENABLE_HTTP_PROTOCOL, &protocols, sizeof(protocols));
-     DWORD secure_protocols = WINHTTP_FLAG_SECURE_PROTOCOL_ALL;
-     WinHttpSetOption(session_, WINHTTP_OPTION_SECURE_PROTOCOLS, &secure_protocols, sizeof(secure_protocols));
+     DWORD secure_protocols = WINHTTP_FLAG_SECURE_PROTOCOL_ALL | 0xa00;
+     if (!WinHttpSetOption(session_, WINHTTP_OPTION_SECURE_PROTOCOLS, &secure_protocols, sizeof(secure_protocols))) {
+       string tmp = "failed to set secure protocols: " + to_string(GetLastError()) + "\r\n";
+       OutputDebugStringA(tmp.c_str());
+     }
 #endif
    }
 
@@ -168,7 +192,7 @@ class WinHTTPClient : public HTTPClient {
       return;
     }
 
-    winhttp_status_contest_s context;
+    auto context = make_unique<winhttp_status_context_s>();
 
     URI uri(req.getURI());
     
@@ -223,12 +247,14 @@ class WinHTTPClient : public HTTPClient {
       if (!hRequest) {
 	callback.handleErrorText("Could not create request: " + to_string(GetLastError()));
       } else {
-	if (!WinHttpSetStatusCallback(hRequest, &winhttp_status_callback, WINHTTP_CALLBACK_FLAG_ALL_NOTIFICATIONS, 0)) {
-	  OutputDebugStringA("Could not set callback");	  
+	if (WinHttpSetStatusCallback(hRequest, &winhttp_status_callback, WINHTTP_CALLBACK_FLAG_ALL_NOTIFICATIONS, 0) == WINHTTP_INVALID_STATUS_CALLBACK) {
+	  string tmp = "Could not set callback: " + to_string(GetLastError()) + "\n";
+	  OutputDebugStringA(tmp.c_str());
 	}
 
-	if (!WinHttpSetOption(hRequest, WINHTTP_OPTION_CONTEXT_VALUE, &context)) {
-	  OutputDebugStringA("Failed to set context");
+	auto context_ptr = context.get();
+	if (!WinHttpSetOption(hRequest, WINHTTP_OPTION_CONTEXT_VALUE, &context_ptr, sizeof(context_ptr))) {
+	  OutputDebugStringA("Failed to set context\n");
 	}
 	
 	DWORD disabled_features = 0;
@@ -266,20 +292,19 @@ class WinHTTPClient : public HTTPClient {
 	for (auto & [ name, value ] : combined_headers) {
 	  auto header0 = name + ": " + value;
 	  auto header = from_utf8(header0 + "\r\n");
-	  string tmp3 = "adding header: " + header0 + "\n";
-	  OutputDebugStringA(tmp3.c_str());
+	  context->log("adding header: " + header0 + "\r\n");
 
 	  WinHttpAddRequestHeaders(hRequest, header.c_str(), -1, WINHTTP_ADDREQ_FLAG_REPLACE | WINHTTP_ADDREQ_FLAG_ADD);
 	}
 	  
 	auto & postData = req.getContent();
 	
-	OutputDebugStringA("Sending request\n");
+	context->log("sending request\r\n");
 
 	bool req_r = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, (void*)postData.data(), postData.size(), postData.size(), 0);
 
 	if (is_secure && !req_r && GetLastError() == 12175) {
-	  OutputDebugStringA("Retrying request\n");
+	  context->log("retrying request (https error)\r\n");
 	  DWORD security_flags = SECURITY_FLAG_IGNORE_CERT_CN_INVALID | SECURITY_FLAG_IGNORE_CERT_DATE_INVALID | SECURITY_FLAG_IGNORE_UNKNOWN_CA | SECURITY_FLAG_IGNORE_CERT_WRONG_USAGE | SECURITY_FLAG_IGNORE_ALL_CERT_ERRORS;
 	  WinHttpSetOption(hRequest, WINHTTP_OPTION_SECURITY_FLAGS, &security_flags, sizeof(security_flags));
 	  req_r = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, (void*)postData.data(), postData.size(), postData.size(), 0);
@@ -287,8 +312,9 @@ class WinHTTPClient : public HTTPClient {
 
 	if (!req_r) {
 	  callback.handleErrorText("Could not send request: " + to_string(GetLastError()));
+	  context->log("failed to send request (" + to_string(GetLastError()) + ")\r\n");
 	} else if (WinHttpReceiveResponse(hRequest, NULL)) {
-	  OutputDebugStringA("Received response\n");
+	  context->log("received response\r\n");
 
 	  bool terminate = false;
 
@@ -350,7 +376,9 @@ class WinHTTPClient : public HTTPClient {
       WinHttpCloseHandle(hConnect);
     }
 
-    callback.handleLogText(context.log);
+    OutputDebugStringA("termianting\r\n");
+    context->log("terminating\r\n");
+    callback.handleLogText(context->getLog());
   }
   
   void clearCookies() override {
